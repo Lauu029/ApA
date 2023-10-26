ed85b34c46f5db5e3ec10eb8cf0107b017849691
Laura <lgomez25@ucm.es>
2023-10-26 17:42:26 +0200

    ai

--- P2/2Practica/multi_linear_reg.py
+++ P2/2Practica/multi_linear_reg.py
@@ -18,7 +18,6 @@ def zscore_normalize_features(X):
     X_Norm = (X-mu)/sigma
     return (X_Norm, mu, sigma)
 
-
 def compute_cost(X, y, w, b):
     """
     compute cost
@@ -30,15 +29,28 @@ def compute_cost(X, y, w, b):
     Returns
       cost (scalar)    : cost
     """
-    m = len(X)
-    cost = 0.0
+    """m = len(X)
+    total_cost = 0.0
     
     for i in range(m):
-           cost += ((w * X[i] + b) - y[i]) ** 2
+        total_cost += ((w * X[i] + b) - y[i]) ** 2
         
-    cost = (1 / (2 * m)) * cost
+    total_cost = (1 / (2 * m)) * total_cost"""
+    m = len(X)
+    total_cost = 0.0
     
-    return cost
+    for i in range(m):
+        temp =0
+        for j in range (len(w)):
+            temp+=w[j]*X[i][j]
+        
+        temp+=b
+        total_cost += (temp - y[i]) ** 2
+        
+    total_cost = (1 / (2 * m)) * total_cost
+    
+    print(total_cost)
+    return total_cost
 
 
 def compute_gradient(X, y, w, b):
@@ -53,23 +65,50 @@ def compute_gradient(X, y, w, b):
       dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b. 
       dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w. 
     """
-    m = len(X)
-    dj_db = (1 / m) * np.sum(w * X + b - y)
-    dj_dw = (1 / m) * np.sum((w * X + b - y) * X, axis=0)
-    
+    m = len(y)
+    dj_dw_temp = (1/m) * np.dot(X.T, (np.dot(X, w) + b)-y)
+    dj_db = (1/m) * np.sum( (np.dot(X, w)+b)-y)
+  
+    dj_dw = dj_dw_temp.flatten()
     return dj_db, dj_dw
 
 def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking
+    num_iters gradient steps with learning rate alpha
+    Args:
+    X : (array_like Shape (m,n) matrix of examples
+    y : (array_like Shape (m,)) target value of each example
+    w_in : (array_like Shape (n,)) Initial values of parameters of the
+    model
+    b_in : (scalar) Initial value of parameter of the model
+    cost_function: function to compute cost
+    2
+    gradient_function: function to compute the gradient
+    alpha : (float) Learning rate
+    num_iters : (int) number of iterations to run gradient descent
+    Returns
+    w : (array_like Shape (n,)) Updated values of parameters of the model
+    after running gradient descent
+    b : (scalar) Updated value of parameter of the model
+    after running gradient descent
+    J_history : (ndarray): Shape (num_iters,) J at each iteration,
+    primarily for graphing later
+    """
+    m, n = X.shape
     w = w_in
     b = b_in
-    J_history = []
+    J_history =np.zeros(num_iters)
 
     for i in range(num_iters):
+        # Calcular el costo y el gradiente utilizando las funciones proporcionadas
         cost = cost_function(X, y, w, b)
         dj_db, dj_dw = gradient_function(X, y, w, b)
-        
+
+        # Actualizar los parámetros w y b
         w -= alpha * dj_dw
         b -= alpha * dj_db
-        
+
+        J_history[i]=cost
 
     return w, b, J_history
